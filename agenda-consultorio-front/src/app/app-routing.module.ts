abe2f281441a1af9ce078f64bda90e3239476084
Eduardo Duarte <[eduardoduarte48@gmail.com]>
2019-12-30 21:56:59 -0300

    Validacao da data no front-end

--- agenda-consultorio-front/src/app/app-routing.module.ts
+++ agenda-consultorio-front/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AgendaComponent } from './agenda/agenda.component';
+import { AgendaCadastroComponent } from './agenda-cadastro/agenda-cadastro.component';
 import { AgendaConsultasComponent } from './agenda-consultas/agenda-consultas.component';
 
 
 const routes: Routes = [
-  { path: '', component: AgendaConsultasComponent }
+  { path: '', component: AgendaConsultasComponent },
+  { path: 'cadastro', component: AgendaCadastroComponent }
 ];
 
 @NgModule({
